dbf473d28620a1dd4330c84996d643d30e51524d
Rebeca <113595025+rsanchez2021@users.noreply.github.com>
2023-11-23 14:20:34 +0100

    Function 1 and 2
    Functions 1 and 2 complete except reset button

--- p3_empotrados.ino
+++ p3_empotrados.ino
@@ -3,6 +3,7 @@
 #include <Thread.h>
 #include <StaticThreadController.h>
 #include <ThreadController.h>
+#include <TimerOne.h>
 
 Thread myThread = Thread();
 
@@ -11,9 +12,9 @@ Thread myThread = Thread();
 #define DHTTYPE DHT11
 DHT dht(DHTPIN, DHTTYPE);
 
-// LED1 -> PWM
-const int LED1_PIN = 10;
-const int LED2_PIN = A2;
+// LED2 -> PWM
+const int LED1_PIN = A2;
+const int LED2_PIN = 10;
 
 // BUTTON
 const int BUTTON_PIN = 9;
@@ -39,12 +40,22 @@ int joyState = 0;
 int sum_random = 0;
 int intense = 0;
 int position = 1;
+int time = 0;
+
+String name1 = "Cafe solo";
 float coffe1 = 1;
+String name2 = "Cafe cortado";
 float coffe2 = 1.10;
+String name3 = "Cafe doble";
 float coffe3 = 1.25;
+String name4 = "Cafe premium";
 float coffe4 = 1.50;
+String name5 = "Chocolate";
 float coffe5 = 2.00;
+
 long randNumber;
+
+bool show_t_h = true;
 
 void setup() {
   Serial.begin(9600);    //iniciar puerto serie
@@ -63,15 +74,15 @@ void setup() {
   // Joystick
   pinMode(JOY_BUTTON, INPUT_PULLUP);
 
-  lcd.clear();
-
   // Thread
   myThread.enabled = true;
   myThread.setInterval(2000);
   myThread.onRun(callback_ultrasonic);
-  // Boot function
-  //Boot();
 
+  lcd.clear();
+
+  // Boot function
+  // Boot();
 }
 
 void callback_ultrasonic(){
@@ -84,6 +95,7 @@ void callback_ultrasonic(){
   d = t/59;             //escalamos el tiempo a una distancia en cm
   if(d >= Distance){
     sleep_mode = true;
+    show_t_h = true; //cuando vuelva el cliente se mostrará la temeperatura
   }
   else{
     sleep_mode = false;
@@ -93,6 +105,7 @@ void callback_ultrasonic(){
 }
 
 void loop(){
+  
   // Compruebo el ultrasonido
   if(myThread.shouldRun()){
     myThread.run();
@@ -101,10 +114,18 @@ void loop(){
   lcd.clear();
 
   if(sleep_mode == true){
-    lcd.print("ESPERANDO CLIENTE");
+    lcd.setCursor(0,0);
+    lcd.print("ESPERANDO");
+    lcd.setCursor(0, 1);
+    lcd.print("CLIENTE");
   }
 
   else if (sleep_mode == false){
+    // Muestro la temperatura si vuelvo al menú
+    if (show_t_h == true){
+      show_tem_hum();
+    }
+
     // Compruebo si el joystick se ha movido hacia un lado o hacia otro
     // Se ha movido hacia la derecha
     if (analogRead(Y_PIN) < 300){
@@ -132,33 +153,43 @@ void loop(){
   }
 }
 
+void show_tem_hum(){
+  unsigned long endTime = millis() + 5000;
+  // Lo meto dentro del while para que se actualice
+  while( millis() < endTime){
+    float h = dht.readHumidity();
+    float t = dht.readTemperature();
+
+    if (isnan(h) || isnan(t)) {
+      Serial.println(F("Failed to read from DHT sensor!"));
+      return;
+    }
+    lcd.setCursor(0,0);
+    lcd.print("Hum: ");
+    lcd.print(h);
+    lcd.setCursor(0,1);
+    lcd.print("Temp: ");
+    lcd.print(t);
+  }
+  show_t_h = false;
+}
+
 void main_menu(){
   switch (position) {
     case 1:
-    //show cafe 1€
-      lcd.print("Cafe solo");
-      lcd.setCursor(0,1);
-      lcd.print(coffe1);
+      show_menu(name1,coffe1);
       break;
     case 2:
-      lcd.print("Cafe cortado");
-      lcd.setCursor(0,1);
-      lcd.print(coffe2);
+      show_menu(name2,coffe2);
       break;
     case 3:
-      lcd.print("Cafe doble");
-      lcd.setCursor(0,1);
-      lcd.print(coffe3);
+      show_menu(name3,coffe3);
       break;
     case 4:
-      lcd.print("Cafe premium");
-      lcd.setCursor(0,1);
-      lcd.print(coffe4);
+      show_menu(name4,coffe4);
       break;
     case 5:
-      lcd.print("Chocolate");
-      lcd.setCursor(0,1);
-      lcd.print(coffe5);
+      show_menu(name5,coffe5);
       break;
   }
 }
@@ -179,11 +210,23 @@ void serve_coffe(){
   Serial.println(randNumber);
   sum_random = 255 / randNumber; // Lo que tiene que sumar cada segundo
   intense = 0;
-  t = 0;
-  while (t < randNumber){
-    digitalWrite(LED1_PIN, intense);
+  lcd.print("PREPARANDO");
+  lcd.setCursor(0,1);
+  lcd.print("CAFE ...");
+  
+  for( int t = 0; t < randNumber; t++){
+    analogWrite(LED2_PIN, intense);
     intense += sum_random;
-    t += 1;
+    delay(1000);
   }
+  analogWrite(LED2_PIN, 0);
+  lcd.clear();
+  lcd.print("RETIRE BEBIDA");
+  delay(3000);
 }
 
+void show_menu(String name, float price){
+  lcd.print(name);
+  lcd.setCursor(0,1);
+  lcd.print(price);
+}
