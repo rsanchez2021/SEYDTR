00fef7df42a41d289e8bbbb9d20a7973ff62fb78
Rebeca <113595025+rsanchez2021@users.noreply.github.com>
2023-11-25 19:49:42 +0100

    admin menu and some functions
    Made the admin menu and some functions with a thread controller

--- p3_empotrados.ino
+++ p3_empotrados.ino
@@ -5,7 +5,10 @@
 #include <ThreadController.h>
 #include <TimerOne.h>
 
-Thread myThread = Thread();
+ThreadController controller = ThreadController();
+Thread tem_Thread = Thread();
+Thread hum_Thread = Thread();
+Thread ult_Thread = Thread(); //Thread del ultrasonidos
 
 // DHT11
 #define DHTPIN 13
@@ -28,6 +31,16 @@ const int Distance = 20; // Distancia cliente
 
 //Crear el objeto LCD con los números correspondientes (rs, en, d4, d5, d6, d7)
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+byte customChar[] = {
+  B00000,
+  B01110,
+  B01010,
+  B01110,
+  B00000,
+  B00000,
+  B00000,
+  B00000
+};
 
 // Joystick
 int JOY_BUTTON = 8;
@@ -40,7 +53,11 @@ int joyState = 0;
 int sum_random = 0;
 int intense = 0;
 int position = 1;
+int position_admin = 1;
 int time = 0;
+int distance = 0;
+float temperature = 0;
+float humidity = 0;
 
 String name1 = "Cafe solo";
 float coffe1 = 1;
@@ -56,33 +73,54 @@ float coffe5 = 2.00;
 long randNumber;
 
 bool show_t_h = true;
+bool admin = true;
+bool loop_tem_hum = false;
+bool loop_sen = false;
+bool loop_count = false;
 
 void setup() {
   Serial.begin(9600);    //iniciar puerto serie
   dht.begin();
+
   // LEDs
   pinMode(LED1_PIN , OUTPUT);
   pinMode(LED2_PIN , OUTPUT);
+
   // BUTTON
   pinMode(BUTTON_PIN, INPUT_PULLUP);
+
   //Ultrasonic
   pinMode(Trigger, OUTPUT);
   pinMode(Echo, INPUT);
   digitalWrite(Trigger, LOW);
   // Inicializar el LCD con el número de  columnas y filas del LCD
   lcd.begin(16, 2);
+  lcd.createChar(0, customChar);
+
   // Joystick
   pinMode(JOY_BUTTON, INPUT_PULLUP);
 
-  // Thread
-  myThread.enabled = true;
-  myThread.setInterval(2000);
-  myThread.onRun(callback_ultrasonic);
+  // Thread ultrasonic (no en el controlador)
+  ult_Thread.enabled = true;
+  ult_Thread.setInterval(2000);
+  ult_Thread.onRun(callback_ultrasonic);
+
+  tem_Thread.enabled = true;
+  tem_Thread.setInterval(500); //Cada medio segundo
+  tem_Thread.onRun(callback_temperature);
+
+  hum_Thread.enabled = true;
+  hum_Thread.setInterval(550); //Cada medio segundo
+  hum_Thread.onRun(callback_humidity);
+
+  controller.add(&tem_Thread);
+  controller.add(&hum_Thread);
 
   lcd.clear();
 
   // Boot function
   // Boot();
+  
 }
 
 void callback_ultrasonic(){
@@ -104,11 +142,29 @@ void callback_ultrasonic(){
   // Comprobar la distancia, si la distancia es mayor de 1m -> modo suspnsión sino vuelvo al loop y muestro menú
 }
 
+void callback_temperature(){
+  Serial.println("Temperatura");
+  lcd.setCursor(0,0);
+  temperature = dht.readTemperature();
+  lcd.print("Temp: ");
+  lcd.print(temperature);
+  lcd.print("C");
+}
+
+void callback_humidity(){
+  humidity = dht.readHumidity();
+  lcd.setCursor(0,1);
+  lcd.print("Hum: ");
+  lcd.print(humidity);
+  lcd.print("%");
+}
+
 void loop(){
-  
+  loop_admin(); // Meter una variable, si es verdad ir a admin con interrupciones
+
   // Compruebo el ultrasonido
-  if(myThread.shouldRun()){
-    myThread.run();
+  if(ult_Thread.shouldRun()){
+    ult_Thread.run();
   }
 
   lcd.clear();
@@ -127,7 +183,7 @@ void loop(){
     }
 
     // Compruebo si el joystick se ha movido hacia un lado o hacia otro
-    // Se ha movido hacia la derecha
+    // Se ha movido hacia arriba
     if (analogRead(Y_PIN) < 300){
       position += 1;
       delay(500); // Añadimos un delay para que no explote
@@ -135,7 +191,7 @@ void loop(){
         position = 1;
       }
     }
-    // Se ha movido el joystick a la izquierda
+    // Se ha movido el joystick abajo
     else if (analogRead(Y_PIN) > 700){
       position -= 1;
       delay(500); // Añadimos un delay para que no explote
@@ -157,19 +213,19 @@ void show_tem_hum(){
   unsigned long endTime = millis() + 5000;
   // Lo meto dentro del while para que se actualice
   while( millis() < endTime){
-    float h = dht.readHumidity();
-    float t = dht.readTemperature();
+    float humidity = dht.readHumidity();
+    temperature = dht.readTemperature();
 
-    if (isnan(h) || isnan(t)) {
+    if (isnan(humidity) || isnan(temperature)) {
       Serial.println(F("Failed to read from DHT sensor!"));
       return;
     }
     lcd.setCursor(0,0);
     lcd.print("Hum: ");
-    lcd.print(h);
+    lcd.print(humidity);
     lcd.setCursor(0,1);
     lcd.print("Temp: ");
-    lcd.print(t);
+    lcd.print(temperature);
   }
   show_t_h = false;
 }
@@ -229,4 +285,113 @@ void show_menu(String name, float price){
   lcd.print(name);
   lcd.setCursor(0,1);
   lcd.print(price);
+}
+
+void loop_admin(){
+  analogWrite(LED2_PIN, 255);
+  digitalWrite(LED1_PIN, HIGH);
+  while (admin){
+
+    lcd.clear();
+
+    if (analogRead(Y_PIN) < 300){
+      position_admin += 1;
+      delay(500); // Añadimos un delay para que no explote
+      if (position_admin > 5){
+        position_admin = 1;
+      }
+    }
+    // Se ha movido el joystick abajo
+    else if (analogRead(Y_PIN) > 700){
+      position_admin -= 1;
+      delay(500); // Añadimos un delay para que no explote
+      if (position_admin < 1){
+        position_admin = 4;
+      }
+    } // Joysticj hacia la izquierda -> mover de aquí
+
+    joyState = digitalRead(JOY_BUTTON);
+    // PULLUP -> LOW
+    if (joyState == LOW){
+      switch (position_admin) {
+        case 1:
+          loop_tem_hum = true;
+          lcd.clear();
+          tem_hum_loop();
+          break;
+        case 2:
+          loop_sen = true;
+          lcd.clear();
+          sen_loop();
+          break;
+        case 3:
+          loop_count = true;
+          count_loop();
+          break;
+        case 4:
+          //price_loop();
+          break;
+  }
+    }
+    admin_menu();
+  }
+}
+
+
+void admin_menu(){
+  switch (position_admin) {
+    case 1:
+      lcd.print("Ver Temperatura");
+      break;
+    case 2:
+      lcd.print("Ver distancia sensor");
+      break;
+    case 3:
+      lcd.print("Ver contador");
+      break;
+    case 4:
+      lcd.print("Modificar precios");
+      break;
+  }
+}
+
+void tem_hum_loop(){
+  while (loop_tem_hum){
+    controller.run();
+    if (analogRead(X_PIN) < 300){
+      loop_tem_hum = false;
+    }
+  }
+}
+
+void sen_loop(){
+  while (loop_sen){
+    lcd.clear();
+    digitalWrite(Trigger, HIGH);
+    delayMicroseconds(10);          //Enviamos un pulso de 10us
+    digitalWrite(Trigger, LOW);
+
+    t = pulseIn(Echo, HIGH); //obtenemos el ancho del pulso
+    d = t/59;
+
+    lcd. print("Distancia: ");
+    lcd.print(distance);
+    lcd.print("cm");
+
+    if (analogRead(X_PIN) < 300){
+      loop_sen = false;
+    }
+  }
+}
+
+void count_loop(){
+  while (loop_count){
+    lcd.clear();
+    time = millis() / 1000;
+    lcd.print(time);
+
+    if (analogRead(X_PIN) < 300){
+      loop_count = false;
+    }
+  }
 }
